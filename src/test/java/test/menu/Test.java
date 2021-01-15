f29eda2162b66ff846913668dc140ffa9a035e10
vincenttuan <vincentjava@yahoo.com.tw>
2021-01-15 17:03:33 +0800

    SpringData

--- src/test/java/test/menu/Test.java
+++ src/test/java/test/menu/Test.java
@@ -1,7 +1,12 @@
 package test.menu;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
-    public static void main(String[] args) {
-        
+
+    @org.junit.Test
+    public void t1() {
+        ClassPathXmlApplicationContext ctx = 
+                new ClassPathXmlApplicationContext("springdata-jpa-config.xml");
     }
 }
